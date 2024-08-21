3d16e3ddee2621dadcd61af73e2f8fd58138c0f4
KeyairaA <kausti2@uic.edu>
2024-08-20 20:56:42 -0500

    Navbar.jsx logo pathing fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import logo from "../assets/keyairaAustinLogo.png";
+import logo from "../assets/KeyairaAustinLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 
