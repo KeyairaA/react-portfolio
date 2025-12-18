dac350021fa1c641a1264f2c2595411794517076
Keyaira <46973441+KeyairaA@users.noreply.github.com>
2025-12-18 13:08:15 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,52 +1,76 @@
 import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
 
-export const HERO_CONTENT = `I am currently an sales specialist aiming to transition into a software engineering role. With three years of hands-on experience as an IT specialist, I\’ve cultivated a deep understanding of technology and problem-solving. Now, I\’m channeling that expertise into my next exciting challenge: becoming a software engineer. Through rigorous self-improvement and a passion for innovation, I’m ready to leverage my IT background to create impactful software solutions.`;
+export const HERO_CONTENT = `I am a passionate full stack developer with a knack for crafting robust and scalable web applications. With 5 years of hands-on experience, I have honed my skills in front-end technologies like React and Next.js, as well as back-end technologies like Node.js, MySQL, PostgreSQL, and MongoDB. My goal is to leverage my expertise to create innovative solutions that drive business growth and deliver exceptional user experiences.`;
 
-export const ABOUT_TEXT = `I am an sales specialist aiming to transition into a software engineering role with three years of experience in the field. I graduated in 2021 with a Bachelor's degree in Mathematics and Computer Science, which provided me with a strong foundation in analytical thinking, problem-solving, and technical skills.`;
+export const ABOUT_TEXT = `I am a dedicated and versatile full stack developer with a passion for creating efficient and user-friendly web applications. With 5 years of professional experience, I have worked with a variety of technologies, including React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB. My journey in web development began with a deep curiosity for how things work, and it has evolved into a career where I continuously strive to learn and adapt to new challenges. I thrive in collaborative environments and enjoy solving complex problems to deliver high-quality solutions. Outside of coding, I enjoy staying active, exploring new technologies, and contributing to open-source projects.`;
 
 export const EXPERIENCES = [
   {
-    year: "2021 - 2025",
-    role: "Technology Specialist I",
-    company: "Gibsons Restaurant Group",
-    description: `I was a level 1 IT specialist that provided comprehensive IT help desk support, troubleshooting and resolving technical issues efficiently to the users within the organization.`,
-    technologies: ["Active Directory", "Google Workspace", "Google Cloud"],
+    year: "2023 - Present",
+    role: "Senior Full Stack Developer",
+    company: "Google Inc.",
+    description: `Led a team in developing and maintaining web applications using JavaScript, React.js, and Node.js. Implemented RESTful APIs and integrated with MongoDB databases. Collaborated with stakeholders to define project requirements and timelines.`,
+    technologies: ["Javascript", "React.js", "Next.js", "mongoDB"],
   },
   {
-    year: "2025 - Present",
-    role: "Specialist",
-    company: "Apple",
-    description: `I am a customer focused Sales Specialist that delivers exceptional retail experiences in a fast paced environment using my extensive knowledge of Apple's products and ecosystem.`,
+    year: "2022 - 2023",
+    role: "Frontend Developer",
+    company: "Adobe",
+    description: `Designed and developed user interfaces for web applications using Next.js and React. Worked closely with backend developers to integrate frontend components with Node.js APIs. Implemented responsive designs and optimized frontend performance.`,
+    technologies: ["HTML", "CSS", "Vue.js", "mySQL"],
+  },
+  {
+    year: "2021 - 2022",
+    role: "Full Stack Developer",
+    company: "Facebook",
+    description: `Developed and maintained web applications using JavaScript, React.js, and Node.js. Designed and implemented RESTful APIs for data communication. Collaborated with cross-functional teams to deliver high-quality software products on schedule.`,
+    technologies: ["Python", "Svelte", "Three.js", "Postgres"],
+  },
+  {
+    year: "2020 - 2021",
+    role: "Software Engineer",
+    company: "Paypal",
+    description: `Contributed to the development of web applications using JavaScript, React.js, and Node.js. Managed databases and implemented data storage solutions using MongoDB. Worked closely with product managers to prioritize features and enhancements.`,
+    technologies: ["Ruby", "Rails", "PHP", "Sqlite"],
   },
 ];
 
 export const PROJECTS = [
   {
-    title: "File Sorter",
+    title: "E-Commerce Website",
     image: project1,
     description:
-      "A python script that automatically sorts files based on their type.",
-    technologies: ["Python"],
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
   },
   {
-    title: "WNBA Stats Analyzer",
+    title: "Task Management App",
     image: project2,
     description:
-      "A python script that scrapes data from wnba.com/stats and analyses it.",
-    technologies: ["Python"],
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
   },
   {
     title: "Portfolio Website",
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "Tailwind CSS", "React"],
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
   },
 ];
 
 export const CONTACT = {
-  phoneNo: "+1 630 835 2041 ",
-  email: "keyaira.austin@gmail.com",
+  address: "767 Fifth Avenue, New York, NY 10153 ",
+  phoneNo: "+12 4555 666 00 ",
+  email: "me@example.com",
 };
