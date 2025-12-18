2901e49d70dafda9165c115949d795043bb23942
Keyaira <46973441+KeyairaA@users.noreply.github.com>
2025-12-18 13:07:04 -0600

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,52 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+
+export const HERO_CONTENT = `I am currently an sales specialist aiming to transition into a software engineering role. With three years of hands-on experience as an IT specialist, I\’ve cultivated a deep understanding of technology and problem-solving. Now, I\’m channeling that expertise into my next exciting challenge: becoming a software engineer. Through rigorous self-improvement and a passion for innovation, I’m ready to leverage my IT background to create impactful software solutions.`;
+
+export const ABOUT_TEXT = `I am an sales specialist aiming to transition into a software engineering role with three years of experience in the field. I graduated in 2021 with a Bachelor's degree in Mathematics and Computer Science, which provided me with a strong foundation in analytical thinking, problem-solving, and technical skills.`;
+
+export const EXPERIENCES = [
+  {
+    year: "2021 - 2025",
+    role: "Technology Specialist I",
+    company: "Gibsons Restaurant Group",
+    description: `I was a level 1 IT specialist that provided comprehensive IT help desk support, troubleshooting and resolving technical issues efficiently to the users within the organization.`,
+    technologies: ["Active Directory", "Google Workspace", "Google Cloud"],
+  },
+  {
+    year: "2025 - Present",
+    role: "Specialist",
+    company: "Apple",
+    description: `I am a customer focused Sales Specialist that delivers exceptional retail experiences in a fast paced environment using my extensive knowledge of Apple's products and ecosystem.`,
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "File Sorter",
+    image: project1,
+    description:
+      "A python script that automatically sorts files based on their type.",
+    technologies: ["Python"],
+  },
+  {
+    title: "WNBA Stats Analyzer",
+    image: project2,
+    description:
+      "A python script that scrapes data from wnba.com/stats and analyses it.",
+    technologies: ["Python"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "Tailwind CSS", "React"],
+  },
+];
+
+export const CONTACT = {
+  phoneNo: "+1 630 835 2041 ",
+  email: "keyaira.austin@gmail.com",
+};
